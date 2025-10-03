07f7a491e13b95debad77ca6505c5a8f4d536fe7
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-10-03 18:29:11 +0200

    add projects

---
+++ FastFood/Program.cs
@@ -0,0 +1,31 @@
+﻿int foodQuantity = Int32.Parse(Console.ReadLine());
+int[] orderQuantity = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+var queue = new Queue<int>(orderQuantity);
+
+Console.WriteLine(queue.Max());
+
+bool isFoodNotEnough = false;
+
+foreach (var order in new Queue<int>(queue))
+{
+    if (order <= foodQuantity)
+    {
+        queue.Dequeue();
+        foodQuantity -= order;
+    }
+    else
+    {
+        isFoodNotEnough = true;
+        break;
+    }
+}
+
+if (isFoodNotEnough)
+{
+    Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+}
+else
+{
+    Console.WriteLine("Orders complete");
+}
