6ea078e14b9095726730bb0fd61bbace9deb33f9
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-10-06 21:20:35 +0200

    add project

---
+++ BalancedParenthesis/Program.cs
@@ -0,0 +1,42 @@
+﻿string inputParentheses = Console.ReadLine();
+char[] sequenceOfParentheses = inputParentheses.ToCharArray();
+
+bool areBalanced = true;
+
+var stack = new Stack<char>();
+
+foreach (char bracket in sequenceOfParentheses)
+{
+    if (bracket == '(' || bracket == '[' || bracket == '{')
+    {
+        stack.Push(bracket);
+    }
+    else
+    {
+        if (stack.Count == 0)
+        {
+            areBalanced = false;
+            break;
+        }
+        else if ((stack.Peek() == '(' && bracket == ')') ||
+           (stack.Peek() == '[' && bracket == ']') ||
+            (stack.Peek() == '{' && bracket == '}'))
+        {
+            stack.Pop();
+        }
+        else
+        {
+            areBalanced = false;
+            break;
+        }
+    }
+}
+
+if (areBalanced)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,8 @@
+﻿string input = Console.ReadLine();
+char[] collection = input.ToCharArray();
+
+var stack = new Stack<char>(collection);
+stack.Pop();
+var queue = new Queue<char>(collection);
+queue.Dequeue();
+Console.WriteLine();
