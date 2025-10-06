50840892745ea53c5ec9b777ef7579f24a3bdc8d
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-10-06 18:37:17 +0200

    add next project

--- KeyRevolver/Program.cs
+++ KeyRevolver/Program.cs
@@ -4,6 +4,65 @@ int[] bullets = Console.ReadLine().Split().Select(int.Parse).ToArray();
 int[] locks = Console.ReadLine().Split().Select(int.Parse).ToArray();
 int intelligenceValue = int.Parse(Console.ReadLine());
 
-var sequenceOfBullets =  new Stack<int>(bullets);
+var sequenceOfBullets = new Stack<int>(bullets);
 var sequenceOfLocks = new Queue<int>(locks);
+
+int shots = 0;
+int reloadingCounter = 0;
+
+bool isOutOfBullets = false;
+bool areAllLocksOpen = false;
+
+while (true)
+{
+    if (sequenceOfLocks.Count == 0)
+    {
+        areAllLocksOpen = true;
+        break;
+    }
+
+    if (sequenceOfBullets.Count > 0)
+    {
+        int currentBullet = sequenceOfBullets.Pop();
+        int currentLock = sequenceOfLocks.Peek();
+
+        if (currentBullet <= currentLock)
+        {
+            sequenceOfLocks.Dequeue();
+            Console.WriteLine("Bang!");
+        }
+        else
+        {
+            Console.WriteLine("Ping!");
+        }
+
+        shots++;
+        reloadingCounter++;
+
+        if (reloadingCounter == sizeOfGunBarrel)
+        {
+            if (sequenceOfBullets.Count != 0)
+            {
+                Console.WriteLine("Reloading!");
+                reloadingCounter = 0;
+            }
+        }
+    }
+    else
+    {
+        isOutOfBullets = true;
+        break;
+    }
+}
+
+if (areAllLocksOpen)
+{
+    int bulletCost = shots * priceOfEachBullet;
+    int earnedMoney = intelligenceValue - bulletCost;
+    Console.WriteLine($"{sequenceOfBullets.Count} bullets left. Earned ${earnedMoney}");
+}
+else if (isOutOfBullets)
+{
+    Console.WriteLine($"Couldn't get through. Locks left: {sequenceOfLocks.Count}");
+}
 
---
+++ TruckTour/Program.cs
@@ -0,0 +1,37 @@
+﻿int petrolPumpsCount = int.Parse(Console.ReadLine());
+
+var sequenceOfPumps = new Queue<int[]>();
+
+for (int i = 0; i < petrolPumpsCount; i++)
+{
+    int[] individualPump = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    sequenceOfPumps.Enqueue(individualPump);
+}
+
+int position = 0;
+
+while (true)
+{
+    int fuel = 0;
+
+    foreach (int[] individualPump in sequenceOfPumps)
+    {
+        int litres = individualPump[0];
+        int distance = individualPump[1];
+
+        fuel += litres - distance;
+
+        if (fuel < 0)
+        {
+            sequenceOfPumps.Enqueue(sequenceOfPumps.Dequeue());
+            position++;
+            break;
+        }
+    }
+
+    if (fuel > 0)
+    {
+        Console.WriteLine(position);
+        break;
+    }
+}
