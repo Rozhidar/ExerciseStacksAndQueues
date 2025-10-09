b6b18aeecfc2073964b000b0be5d22624d654950
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-10-09 21:56:18 +0200

    current state of project

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,8 +1,52 @@
-﻿string input = Console.ReadLine();
-char[] collection = input.ToCharArray();
+﻿//string input = Console.ReadLine();
+//char[] collection = input.ToCharArray();
 
-var stack = new Stack<char>(collection);
-stack.Pop();
-var queue = new Queue<char>(collection);
-queue.Dequeue();
-Console.WriteLine();
+//var stack = new Stack<char>(collection);
+//stack.Pop();
+//var queue = new Queue<char>(collection);
+//queue.Dequeue();
+//Console.WriteLine();
+
+//////////////////////////////////////
+///
+
+//string d = "h ellothere";
+//string v = "heyhey";
+
+//string s = d + v;
+//string k = d.Substring(2);
+//Console.WriteLine(k);
+
+////////////////////////////////
+
+//string f = "Hellohey";
+//Console.WriteLine(f[5 - 1]);
+/////////////////////////////
+
+//var stack = new Stack<string>();
+
+//string q = "Hello there";
+//stack.Push(q);
+//Console.WriteLine(stack.Peek());
+
+//string afterOne = q + " you";
+//stack.Push(afterOne);
+//Console.WriteLine(stack.Peek());
+
+
+//string c = "Hello there";
+//int index = 4;
+//Console.WriteLine(c.Substring(0, 1));
+
+
+//for (int i = 0; i < 10; i++)
+//{
+
+//}
+//string res = c.Substring(c.Length - index);
+
+//Console.WriteLine(res);
+//////////////////////////////////////////////
+
+string l = "2, 2";
+
---
+++ SimpleTextEditor/Program.cs
@@ -0,0 +1,53 @@
+﻿int numberOfOperations = int.Parse(Console.ReadLine());
+
+string someString = string.Empty;
+
+var storeLastCommand = new Stack<string>();
+
+for (int i = 0; i < numberOfOperations; i++)
+{
+    string command = Console.ReadLine();
+
+    if (command.StartsWith("1"))
+    {
+        var appendString = command.Substring(2);
+
+        someString += appendString;
+        storeLastCommand.Push(command);
+    }
+    else if (command.StartsWith("2"))
+    {
+        int countOfLastElementsToErase = int.Parse(command.Substring(2));
+
+        someString = someString.Substring(0, someString.Length - countOfLastElementsToErase);
+        storeLastCommand.Push(command);
+    }
+    else if (command.StartsWith("3"))
+    {
+        int index = int.Parse(command.Substring(2));
+        Console.WriteLine(someString[index - 1]);
+    }
+    else if (command.StartsWith("4"))
+    {
+        var lastCommand = storeLastCommand.Pop();
+
+        //if (lastCommand.StartsWith("1"))
+        //{
+        //    var addedText = lastCommand.Substring(2);
+        //    someString = someString.Substring(0, someString.Length - addedText.Length);
+        //}
+        //else if (lastCommand.StartsWith("2"))
+        //{
+        //    var removedText = lastCommand.Substring(2);
+
+        //    someString += removedText;
+        //}
+    }
+}
+
+
+//string result = original.Substring(0, original.Length - n);
+// hello
+// hel
+// lo
+// 
