c1b12b0a12ebfca88a4d16bf8f70cb53e81ad7ad
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-10-10 07:24:25 +0200

    fixed project

--- SimpleTextEditor/Program.cs
+++ SimpleTextEditor/Program.cs
@@ -2,7 +2,7 @@
 
 string someString = string.Empty;
 
-var storeLastCommand = new Stack<string>();
+var storeLastString = new Stack<string>();
 
 for (int i = 0; i < numberOfOperations; i++)
 {
@@ -12,15 +12,15 @@ for (int i = 0; i < numberOfOperations; i++)
     {
         var appendString = command.Substring(2);
 
+        storeLastString.Push(someString);
         someString += appendString;
-        storeLastCommand.Push(command);
     }
     else if (command.StartsWith("2"))
     {
         int countOfLastElementsToErase = int.Parse(command.Substring(2));
 
+        storeLastString.Push(someString);
         someString = someString.Substring(0, someString.Length - countOfLastElementsToErase);
-        storeLastCommand.Push(command);
     }
     else if (command.StartsWith("3"))
     {
@@ -29,25 +29,8 @@ for (int i = 0; i < numberOfOperations; i++)
     }
     else if (command.StartsWith("4"))
     {
-        var lastCommand = storeLastCommand.Pop();
-
-        //if (lastCommand.StartsWith("1"))
-        //{
-        //    var addedText = lastCommand.Substring(2);
-        //    someString = someString.Substring(0, someString.Length - addedText.Length);
-        //}
-        //else if (lastCommand.StartsWith("2"))
-        //{
-        //    var removedText = lastCommand.Substring(2);
-
-        //    someString += removedText;
-        //}
+        someString = storeLastString.Pop();
     }
 }
 
 
-//string result = original.Substring(0, original.Length - n);
-// hello
-// hel
-// lo
-// 
